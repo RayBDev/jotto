35eb0aa926c51795b230d5cc2443e9420baa9510
RayB <raybernard1337@gmail.com>
2019-09-11 06:53:38 -0600

    Added test utils and congrats component tests

---
+++ src/components/Congrats.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component
+ */
+const Congrats = () => {
+  return <div></div>;
+};
+
+export default Congrats;
