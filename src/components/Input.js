bcbc863d782b02e88a5f6cc39957197e8e69e145
RayB <raybernard1337@gmail.com>
2019-09-17 08:23:05 -0600

    Added code for success=false

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,11 +3,30 @@ import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <div></div>;
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          type="text"
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{contents}</div>;
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ success }) => {
+  return { success };
+};
 
 const mapDispatchToProps = {};
 
--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -19,17 +19,32 @@ const setup = (initialState = {}) => {
 };
 
 describe("render", () => {
-  describe("word has not been guessed", () => {
+  describe("word has been guessed", () => {
     test("renders component without error", () => {});
     test("renders input box error", () => {});
     test("renders submit button", () => {});
   });
 
-  describe("word has been guessed", () => {
-    test("renders component without error", () => {});
-    test("does not render input box error", () => {});
-    test("does not render submit button", () => {});
+  describe("word has not been guessed", () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("does not render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("does not render submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
   });
+  describe("update state", () => {});
 });
 
 describe("update state", () => {
