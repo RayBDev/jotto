2d2c6751145eb0b9d530ca4d7e7260370b03d914
RayB <raybernard1337@gmail.com>
2019-09-18 08:18:02 -0600

    Added Redux Thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,17 +1,21 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../src/store/reducers";
+import { middleWares } from "../src/store/configureStore";
 
 /**
  * Creating a testing store with imported reducers, middleware, and initial state.
- * globals: rootReducer
+ * globals: rootReducer, middleWares
  * @param {object} initialState - Initial state for store
  * @function storeFactory
  * @returns {Store} - Redux store
  */
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middleWares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 /**
